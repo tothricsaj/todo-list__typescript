c1e72bcb6cc7491a565b298320a9be0a6cea9479
Richard Toth <Richard_Toth@epam.com>
2021-04-26 12:05:37 +0200

    (refact): changed favicon and deleted logo.svg

--- src/components/ListContainer/ListContainer.tsx
+++ src/components/ListContainer/ListContainer.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 import ListItem from '../ListItem/ListItem'
 
+import './ListContainer.css'
+
 const listContainer = (props: any) => {
   return (
-    <div>
+    <div className="container">
       <h3>List Container</h3>
       <ListItem />
     </div>
