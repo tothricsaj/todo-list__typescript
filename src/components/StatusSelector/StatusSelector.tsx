5d2841e715e85e84b0db56548f073d407892ac92
Richard Toth <Richard_Toth@epam.com>
2021-04-30 11:34:50 +0200

    (feature): select status button

--- src/components/StatusSelector/StatusSelector.tsx
+++ src/components/StatusSelector/StatusSelector.tsx
@@ -3,17 +3,64 @@ import React from 'react'
 import style from './StatusSelector.module.css'
 
 const StatusSelector = () => {
- return (
-   <div className={style.status_wrapper}>
-    <div
-      className="button button__selected"
-      style={{marginLeft: '0'}}
-    >All</div>
+  const [selectedBtn, setSelectedBtn] =
+    React.useState({
+      all: true,
+      ready: false,
+      deleted: false
+    })
 
-    <div className="button">Ready</div>
-    <div className="button">Deleted</div>
-   </div>
- )
+  const selectBtn = (selected: string) => {
+    switch (selected) {
+      case 'all':
+        setSelectedBtn({
+          all: true,
+          ready: false,
+          deleted: false
+        })
+        break
+      case 'ready':
+        setSelectedBtn({
+          all: false,
+          ready: true,
+          deleted: false
+        })
+        break
+      case 'deleted':
+        setSelectedBtn({
+          all: false,
+          ready: false,
+          deleted: true
+        })
+        break
+      default: 
+        setSelectedBtn({
+          all: true,
+          ready: false,
+          deleted: false
+        })
+    }
+  }
+
+  return (
+    <div className={style.status_wrapper}>
+      <div
+        className={`button ${selectedBtn.all ? 'button__selected':null }`}
+        style={{marginLeft: '0'}}
+        onClick={() => selectBtn('all')}
+      >All</div>
+
+      <div
+        className={`button ${selectedBtn.ready ? 'button__selected':null }`}
+        onClick={() => selectBtn('ready')}
+      >Ready</div>
+
+      <div
+        className={`button ${selectedBtn.deleted ? 'button__selected':null }`}
+        onClick={() => selectBtn('deleted')}
+      >Deleted</div>
+    </div>
+  )
 }
 
 export default StatusSelector
